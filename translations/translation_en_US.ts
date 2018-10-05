d8a6ac5c09c43ec49b4d31663fbac4d0b59cd77f
Antti Rauti <antti.rauti@cs.tamk.fi>
2018-10-05 12:09:55 +0300

    Added a couple behaviors and translations

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>Basic behaviour/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Moro</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Moro</translation>
+        </message>
+        <message>
+            <source>Moro kaikille, olen Pepper</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Moro kaikille, olen Pepper</translation>
+        </message>
+        <message>
+            <source>Moro kaikille, nimeni on pepperi</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Moro kaikille, nimeni on pepperi</translation>
+        </message>
+    </context>
+    <context>
+        <name>Basic behaviour/behavior.xar:/Choice/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Oletpa iloinen</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Oletpa iloinen</translation>
+        </message>
+    </context>
+    <context>
+        <name>Basic behaviour/behavior.xar:/Choice/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Onko kaikki hyvin?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Onko kaikki hyvin?</translation>
+        </message>
+    </context>
+    <context>
+        <name>Basic behaviour/behavior.xar:/Choice/Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>En erota naamaasi</source>
+            <comment>Text</comment>
+            <translation type="obsolete">En erota naamaasi</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+</TS>
